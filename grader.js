4e86143aaa727a1a4645bf432aa9556428088b06
Ubuntu <ubuntu@ip-172-31-42-77.us-west-2.compute.internal>
2013-07-09 15:39:16 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -63,7 +63,9 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-u, --url<url>', 'url to index.html')
+        .option('-u, --url <url>', 'url to index.html')
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)     
         .parse(process.argv);            
     if(program.url){                
         var url = program.url.toString();              
